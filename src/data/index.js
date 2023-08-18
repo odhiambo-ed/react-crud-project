32490379f8105c52d1683ea381cc188b55bac97e
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-18 14:34:23 +0300

    Add Seed Data

---
+++ src/data/index.js
@@ -0,0 +1,74 @@
+const employeeData = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com",
+        address: "123 Main St, City, Country",
+        phone: "123-456-7890"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane.smith@example.com",
+        address: "456 Elm Ave, Town, Country",
+        phone: "987-654-3210"
+    },
+    {
+        id: 3,
+        name: "Michael Johnson",
+        email: "michael.johnson@example.com",
+        address: "789 Oak Rd, Village, Country",
+        phone: "555-555-5555"
+    },
+    {
+        id: 4,
+        name: "Emily Brown",
+        email: "emily.brown@example.com",
+        address: "567 Pine Ln, Suburb, Country",
+        phone: "222-333-4444"
+    },
+    {
+        id: 5,
+        name: "David Lee",
+        email: "david.lee@example.com",
+        address: "890 Maple Dr, City, Country",
+        phone: "888-777-6666"
+    },
+    {
+        id: 6,
+        name: "Sarah Williams",
+        email: "sarah.williams@example.com",
+        address: "345 Birch Rd, Town, Country",
+        phone: "111-222-3333"
+    },
+    {
+        id: 7,
+        name: "James Miller",
+        email: "james.miller@example.com",
+        address: "678 Cedar Ave, Village, Country",
+        phone: "444-555-6666"
+    },
+    {
+        id: 8,
+        name: "Ava Martinez",
+        email: "ava.martinez@example.com",
+        address: "987 Oak St, Suburb, Country",
+        phone: "777-888-9999"
+    },
+    {
+        id: 9,
+        name: "William Taylor",
+        email: "william.taylor@example.com",
+        address: "123 Elm Rd, City, Country",
+        phone: "555-444-3333"
+    },
+    {
+        id: 10,
+        name: "Olivia Jackson",
+        email: "olivia.jackson@example.com",
+        address: "456 Maple Ave, Town, Country",
+        phone: "111-222-3333"
+    }
+];
+
+export default employeeData;
