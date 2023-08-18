066eb553823fa2c4bfef4dc2e2db08c9df6c537d
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-18 13:19:15 +0300

    Add Employee List Component

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,7 +1,13 @@
 
 const Employee = () => {
   return (
-    <div>Employee</div>
+    <div>
+          <ul>
+              <li>Employee 1</li>
+              <li>Employee 2</li>
+              <li>Employee 3</li>
+        </ul>
+    </div>
   )
 }
 
