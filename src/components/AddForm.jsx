1b5ee6e90968450117955b37f019b25e5ad35a9e
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-28 19:23:12 +0300

    Create Add Function

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,13 +1,32 @@
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { useContext, useState } from 'react';
+import { EmployeeListContext } from './contexts/EmployeeListContext';
 
 const AddForm = () => {
+  const { addEmployee } = useContext(EmployeeListContext);
+
+  const [newEmployee, setNewEmployee] = useState({name:"", email:"", address:"", phone:""});
+
+  const handleChange = (e) => {
+    setNewEmployee({ ...newEmployee, [e.target.name]: e.target.value });
+   }
+
+  const { name, email, address, phone } = newEmployee;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEmployee(name, email, address, phone)
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Control
           type="text"
           placeholder="Name"
+          name="name"
+          value={name}
+          onChange={(e) => handleChange(e)}
           className="mb-3"
           required
         />
@@ -16,6 +35,9 @@ const AddForm = () => {
         <Form.Control
           type="email"
           placeholder="Email"
+          name="email"
+          value={email}
+          onChange={(e) => handleChange(e)}
           className="mb-3"
           required
         />
@@ -25,6 +47,9 @@ const AddForm = () => {
           as="textarea"
           rows={3}
           placeholder="Address"
+          name="address"
+          value={address}
+          onChange={(e) => handleChange(e)}
           className="mb-3"
           required
         />
@@ -33,6 +58,9 @@ const AddForm = () => {
         <Form.Control
           type="text"
           placeholder="Phone"
+          name="phone"
+          value={phone}
+          onChange={(e) => handleChange(e)}
           className="mb-3"
           required
         />
