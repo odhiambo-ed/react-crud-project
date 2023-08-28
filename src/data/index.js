733fa763bd80700c2250c25c82a7a72a3b49a128
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-28 19:26:54 +0300

    Add uuid To Data

--- src/data/index.js
+++ src/data/index.js
@@ -1,69 +1,71 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const employeeData = [
     {
-        id: 1,
+        id:uuidv4(),
         name: "John Doe",
         email: "john.doe@example.com",
         address: "123 Main St, City, Country",
         phone: "123-456-7890"
     },
     {
-        id: 2,
+        id:uuidv4(),
         name: "Jane Smith",
         email: "jane.smith@example.com",
         address: "456 Elm Ave, Town, Country",
         phone: "987-654-3210"
     },
     {
-        id: 3,
+        id:uuidv4(),
         name: "Michael Johnson",
         email: "michael.johnson@example.com",
         address: "789 Oak Rd, Village, Country",
         phone: "555-555-5555"
     },
     {
-        id: 4,
+        id: uuidv4(),
         name: "Emily Brown",
         email: "emily.brown@example.com",
         address: "567 Pine Ln, Suburb, Country",
         phone: "222-333-4444"
     },
     {
-        id: 5,
+        id:uuidv4(),
         name: "David Lee",
         email: "david.lee@example.com",
         address: "890 Maple Dr, City, Country",
         phone: "888-777-6666"
     },
     {
-        id: 6,
+        id:uuidv4(),
         name: "Sarah Williams",
         email: "sarah.williams@example.com",
         address: "345 Birch Rd, Town, Country",
         phone: "111-222-3333"
     },
     {
-        id: 7,
+        id:uuidv4(),
         name: "James Miller",
         email: "james.miller@example.com",
         address: "678 Cedar Ave, Village, Country",
         phone: "444-555-6666"
     },
     {
-        id: 8,
+        id:uuidv4(),
         name: "Ava Martinez",
         email: "ava.martinez@example.com",
         address: "987 Oak St, Suburb, Country",
         phone: "777-888-9999"
     },
     {
-        id: 9,
+        id:uuidv4(),
         name: "William Taylor",
         email: "william.taylor@example.com",
         address: "123 Elm Rd, City, Country",
         phone: "555-444-3333"
     },
     {
-        id: 10,
+        id:uuidv4(),
         name: "Olivia Jackson",
         email: "olivia.jackson@example.com",
         address: "456 Maple Ave, Town, Country",
