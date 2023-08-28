13d178ee8aec1d412d4c3709e18e71cfd96544a3
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-08-28 19:23:56 +0300

    Create Add Employee Context

--- src/components/contexts/EmployeeListContext.jsx
+++ src/components/contexts/EmployeeListContext.jsx
@@ -1,17 +1,29 @@
 import { createContext, useEffect, useState } from "react";
 import employeeData from "../../data";
+import { v4 as uuidv4 } from 'uuid';
+
+
+
 export const EmployeeListContext = createContext();
 
 const EmployeeListContextProvider = (props) => {
   const [employees, setEmployees] = useState([employeeData]);
+  
+
+  const addEmployee = (id, name, address, phone) => {
+    setEmployees([...employees, {id:uuidv4(), name, address, phone}]);
+  }
 
   useEffect(() => {
+    // Save to localStorage
     localStorage.setItem("employees", JSON.stringify(employeeData));
+
+    // Retrieve from localStorage and update state
     setEmployees(JSON.parse(localStorage.getItem("employees")));
-  });
+  }, []);
 
   return (
-    <EmployeeListContext.Provider value={{ employees }}>
+    <EmployeeListContext.Provider value={{ employees, addEmployee }}>
       {props.children}
     </EmployeeListContext.Provider>
   );
